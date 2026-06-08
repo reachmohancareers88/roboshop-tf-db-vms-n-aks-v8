91e5b63f0c4d4ca6e80105d948c3d3ea16892c07
reachmohancareers88 <reachmohancareers@gmail.com>
2026-06-08 16:37:10 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -3,5 +3,8 @@
 sudo dnf install python3-pip.noarch -y
 sudo pip3.12 install ansible
 
-ansible-pull -i localhost, -U https://github.com/raghudevopsb89/roboshop-ansible-v4.git roboshop.yml -e component_name=${component_name} -e env=${env}
-
+ansible-pull -i localhost, \
+-U https://github.com/reachmohancareers88/roboshop-ansible-v4.git \
+roboshop.yml \
+-e component_name=${component_name} \
+-e env=${env}
